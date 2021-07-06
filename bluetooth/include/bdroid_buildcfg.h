9d1e4fffca8b852f096f99a48a48d2e04a8502c8
Ihor Ivanov <toulmee@gmail.com>
2021-07-06 15:53:10 +0300

    Remove a lot of unnecessary stuff
    Signed-off-by: Ihor Ivanov <toulmee@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -21,7 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Jell2"
+#define BTM_DEF_LOCAL_NAME   "Jelly2"
 // Disables read remote device feature
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
--- deviceCommon.mk
+++ deviceCommon.mk
@@ -26,136 +26,19 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # A/B
 AB_OTA_UPDATER := false
 
-# Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    libaacwrapper
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
-
-## FMRadio
-#PRODUCT_PACKAGES += \
-#    FMRadio \
-#    libfmcust
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
-# fstab
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/etc/fstab.mt6771:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6771
 
 # NFC stack (AOSP)
 PRODUCT_PACKAGES += \
     NfcNci
 
-# ImsInit hack
-PRODUCT_PACKAGES += \
-    ImsInit
-
-# Init
-PRODUCT_PACKAGES += \
-    init.mt6771.rc \
-    fstab.mt6771
-
-# Magisk
-PRODUCT_PACKAGES += \
-    99-magisk
-
+# fstab
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/a2dp_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_configuration_stub.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration_stub.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/usb_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/hearing_aid_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default_volume_tables.xml
-
-PRODUCT_PACKAGES += \
-    libGLES_android \
-    libaudioprocessing \
-    libaudioflinger \
-    libaudio-resampler \
-    libcameraservice \
-    libsuspend \
-    libstatssocket \
-    libsparse \
-    libdrm \
-    libpcap \
-    libprotobuf-cpp-full \
-    libbrotli \
-    libext2_blkid \
-    libext2_com_err \
-    libext2_e2p \
-    libext2_profile \
-    libext2_uuid \
-    libext2fs \
-    libmetricslogger \
-    com.android.nfc_extras \
-    android.frameworks.sensorservice@1.0 \
-    android.hardware.audio.common-util \
-    android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0 \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@5.0 \
-    android.hardware.audio@2.0 \
-    android.hardware.audio@4.0 \
-    android.hardware.audio@5.0 \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.health@1.0 \
-    android.hardware.health@2.0 \
-    android.hardware.nfc@1.0 \
-    android.hardware.nfc@1.1 \
-    android.hardware.nfc@1.2 \
-    android.hardware.thermal@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.3 \
-    android.hardware.radio@1.4 \
-    android.hardware.vibrator@1.0 \
-    android.hardware.vibrator@1.3
-
-# Additional tools
-PRODUCT_HOST_PACKAGES += \
-    unpack_bootimg \
-    lpmake
-
-# Keylayouts
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
-    $(DEVICE_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl \
-    $(DEVICE_PATH)/keylayout/mtk-tpd-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-tpd-kpd.kl
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
-
-# Overlays -- replace official
-PRODUCT_PACKAGES += \
-    FrameworkResOverlay
-
+    $(DEVICE_PATH)/fstab.mt6771:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6771
 
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
-
-# Telephony Jars
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
 
 #Include GAPPS if applicable
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
